0b8df47c57ab1bba2511f212109876abf53a69b7
Mark Klass <mark_christian@zoho.com>
2022-01-25 16:29:39 +0800

    fixed categorys -> categories. ugly af.

--- docker/code/products/admin.py
+++ docker/code/products/admin.py
@@ -1,6 +1,13 @@
+from django.apps import AppConfig
 from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
+
+
+class CategoryConfig(AppConfig):
+    name = "Category"
+    verbose_name = "Categories"
+
--- docker/code/products/models.py
+++ docker/code/products/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
@@ -13,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    date_created = models.DateTimeField('date created')
+    date_created = models.DateTimeField('date created', default=timezone.now, editable=False)
     date_modified = models.DateTimeField('date modified', default=timezone.now)
 
     def __str__(self):
